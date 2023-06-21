3f18632d51a4268a807fdfd8aa73e1a56a0936eb
Patrick Oser <patrick.oser148@gmail.com>
2023-06-21 11:12:59 -0400

    Added input for players name.

--- app.js
+++ app.js
@@ -9,12 +9,26 @@ const gameModule = (() => {
   };
 })();
 
+const playerInfo = (() => {
+  const playerName = document.getElementById("playerName");
+  const blockedGameBoard = document.getElementById("blocked");
+
+  playerName.addEventListener("input", function () {
+    if (this.inputMode.valueOf.trim() !== "") {
+      blockedGameBoard.id.remove("blocked");
+    } else {
+      blockedGameBoard.id.add("blocked");
+    }
+  });
+})();
+
 const playerFactory = (name) => {
   const sayName = function () {
     console.log(`This is ${name}`);
   };
   return {
     sayName,
+    name,
   };
 };
 
