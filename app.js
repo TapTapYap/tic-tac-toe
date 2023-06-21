78c307b3fe1605a5c8826ef3a9d16b23fc6e5d1a
Patrick Oser <patrick.oser148@gmail.com>
2023-06-21 09:49:27 -0400

    Event listener added to board tiles.

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const gameModule = (() => {
   const movesMade = [];
+  let gameBoardTiles = document.getElementsByClassName("boardTiles");
+  gameBoardTiles.addEventListener("click", function () {
+    // Logic that adds the players value to the board and adds it to the movesMade array
+  });
   return {
     movesMade,
   };
