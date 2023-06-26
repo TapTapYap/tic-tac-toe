550cb8f7d556c312f1997186978ee110034dfeda
Patrick Oser <patrick.oser148@gmail.com>
2023-06-26 12:55:14 -0400

    Add gameModule IIFE to JS file.

--- app.js
+++ app.js
@@ -15,9 +15,37 @@ const gameModule = (() => {
     board.push("");
   }
 
+  let boardTilesContainer = document.querySelector(".board");
   let tiles = document.querySelector(".boardTiles");
 
+  Array.from(boardTilesContainer.children).forEach((tiles, index) => {
+    tiles.addEventListener("click", () => {
+      // Display active payer marker
+      tiles.classList.add(game.activePlayer.marker);
+      tiles.setAttribute("data", game.activePlayer.marker);
+
+      // Update array value to be that of active player
+      board[index] = game.activePlayer.marker;
+
+      // remove event listener from the marked index
+      tiles.getElementsByClassName.pointerEvents = "none";
+
+      // Update remaining spots
+      game.remainingSpots -= 1;
+
+      // Check winner
+      game.checkWinner();
+
+      // Check remaining spots
+      if (game.winnerDeclared == false) {
+        if (game.remainingSpots > 0) {
+          game.alertNextPlayer();
+          game.nextPlayer();
+        } else if (game.remainingSpots == 0) {
+          game.declareTie();
+        }
+      }
+    });
+  });
   return { board };
 })();
-
-console.log(board);
