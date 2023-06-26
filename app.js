31e9398d28991b6d5e8a41e724bfdbcd0f670b34
Patrick Oser <patrick.oser148@gmail.com>
2023-06-26 12:28:57 -0400

    Add array containing moves made on board.

--- app.js
+++ app.js
@@ -1,4 +1,23 @@
+// PLayer factory Function
+const playerFactory = (player, marker) => {
+  return {
+    player,
+    marker,
+  };
+};
+
+// Game board IIFE object
 const gameModule = (() => {
-  const movesMade = [];
-  return {};
+  // generate board array
+  let board = [];
+
+  for (i = 0; i < 9; i++) {
+    board.push("");
+  }
+
+  let tiles = document.querySelector(".boardTiles");
+
+  return { board };
 })();
+
+console.log(board);
