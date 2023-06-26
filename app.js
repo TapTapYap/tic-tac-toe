c88fcb3a058182f90f5a2cb122ca07c60f8f92e7
Patrick Oser <patrick.oser148@gmail.com>
2023-06-26 11:28:31 -0400

    Starting from scratch.

--- app.js
+++ app.js
@@ -1,28 +1,4 @@
 const gameModule = (() => {
   const movesMade = [];
-  let gameBoardTiles = document.getElementsByClassName("boardTiles");
-  gameBoardTiles.addEventListener("click", function () {
-    // Logic that adds the players value to the board and adds it to the movesMade array
-  });
-  return {
-    movesMade,
-  };
+  return {};
 })();
-
-const playerInfo = (() => {
-  const playerName = document.getElementById("playerName");
-  const blockedGameBoard = document.getElementById("blocked");
-})();
-
-const playerFactory = (name) => {
-  const sayName = function () {
-    console.log(`This is ${player}`);
-  };
-  let player = name;
-  let playerValue = value;
-  return {
-    sayName,
-    player,
-    playerValue,
-  };
-};
