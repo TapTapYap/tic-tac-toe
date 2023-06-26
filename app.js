f0e406b7e3c1d26b62c9bb37fc6fe44e5611d7d6
Patrick Oser <patrick.oser148@gmail.com>
2023-06-26 13:50:17 -0400

    Add game IIFE module.

--- app.js
+++ app.js
@@ -49,3 +49,74 @@ const gameModule = (() => {
   });
   return { board };
 })();
+
+// Game IIFE object
+const game = (() => {
+  // declare players
+  const playerOne = createPlayer("Player 1", "X");
+  const PlayerTwo = createPlayer("Player 2", "O");
+
+  // starting point
+  let activePlayer = playerOne;
+  let winnerDeclared = false;
+  let remainingSpots = 9;
+
+  // selectors
+  let subText = document.querySelector(".subtext"); // display winner/tie
+  let playerName = document.querySelector(".player-name"); // purpose: alert player turn
+
+  // winning conditions
+  const winConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  // check winner
+  function checkWinner() {
+    winConditions.forEach((item, index) => {
+      if (
+        gameModule.board[item[0]] === this.activePlayer.marker &&
+        gameModule.board[item[1]] === this.activePlayer.marker &&
+        gameModule.board[item[2]] === this.activePlayer.marker
+      ) {
+        subText.innerHTML = `<b>${this.activePlayer.name} wins!</b>`;
+        this.winnerDeclared = true;
+      }
+    });
+  }
+
+  // alert next player
+  function alertNextPlayer() {
+    this.activePlayer === playerOne
+      ? (playerName.textContent = "Player 2")
+      : (playerName.textContent = "Player 1");
+  }
+
+  // next player
+  function nextPlayer() {
+    this.activePlayer === playerOne
+      ? (this.activePlayer = playerTwo)
+      : (this.activePlayer = playerOne);
+  }
+
+  // declare tie
+  function declareTie() {
+    subText.innerHTML = "<br>Tie game!</br>";
+  }
+
+  return {
+    activePlayer,
+    remainingSpots,
+    checkWinner,
+    alertNextPlayer,
+    nextPlayer,
+    declareTie,
+    winnerDeclared,
+  };
+})();
